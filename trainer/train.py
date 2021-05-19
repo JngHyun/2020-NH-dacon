e7f99a0735c3446799ad644837c7c33fb40a3109
JngHyun <pjh09890989@gmail.com>
2021-05-19 18:06:31 +0900

    feat: cbow trainer

--- trainer/train.py
+++ trainer/train.py
@@ -1,68 +1,76 @@
 import torch
+import time
 
 
-loss_function = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
-train_batches = list(train_iterator)
-valid_batches = list(valid_iterator)
 
-history = defaultdict(list)
+def evaluate_validation(scores, loss_function, labels):
+    preds = scores.argmax(dim=1)
+    n_correct = (preds == labels).sum().item()
+    return n_correct, loss_function(scores, labels).item()
 
-for i in range(200):
-    
-    t0 = time.time()
-    
-    loss_sum = 0
-    n_batches = 0
+def cbow_trainer(self,model,train_iterator,valid_iterator,epoch):
+    self.model = model
+    self.train_iterator = train_iterator
+    self.valid_iterator = valid_iterator
+    self.epoch
 
-    # Calling model.train() will enable the dropout layers.
-    model.train()
-    
-    # We iterate through the batches created by torchtext.
-    # For each batch, we can access the text part and the output label part separately.
-    for batch in train_batches:
+    train_batches = list(self.train_iterator)
+    valid_batches = list(self.valid_iterator)
+
+    loss_function = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0005)
+
+    for i in range(self.epoch):       
+        t0 = time.time()
         
-        # Compute the output scores.
-        scores = model(batch.text)
-        # Then the loss function.
-        loss = loss_function(scores, batch.label)
+        loss_sum = 0
+        n_batches = 0
 
-        # Compute the gradient with respect to the loss, and update the parameters of the model.
-        optimizer.zero_grad()            
-        loss.backward()
-        optimizer.step()
+        model.train()
+        
+        # We iterate through the batches created by torchtext.
+        # For each batch, we can access the text part and the output label part separately.
+        for batch in train_batches:
+            
+            # Compute the output scores.
+            scores = model(batch.text)
+            # Then the loss function.
+            loss = loss_function(scores, batch.label)
 
-        loss_sum += loss.item()
-        n_batches += 1
-    
-    train_loss = loss_sum / n_batches
-    history['train_loss'].append(train_loss)
-    
-    # After each training epoch, we'll compute the loss and accuracy on the validation set.
-    n_correct = 0
-    n_valid = len(valid)
-    loss_sum = 0
-    n_batches = 0
+            # Compute the gradient with respect to the loss, and update the parameters of the model.
+            optimizer.zero_grad()            
+            loss.backward()
+            optimizer.step()
 
-    # Calling model.train() will disable the dropout layers.
-    model.eval()
+            loss_sum += loss.item()
+            n_batches += 1
+        
+        train_loss = loss_sum / n_batches
+        #history['train_loss'].append(train_loss)
+        
+        # After each training epoch, we'll compute the loss and accuracy on the validation set.
+        n_correct = 0
+        n_valid = len(valid)
+        loss_sum = 0
+        n_batches = 0
 
-    for batch in valid_batches:
-        scores = model(batch.text)
-        n_corr_batch, loss_batch = evaluate_validation(scores, loss_function, batch.label)
-        loss_sum += loss_batch
-        n_correct += n_corr_batch
-        n_batches += 1
-    val_acc = n_correct / n_valid
-    val_loss = loss_sum / n_batches
+        # Calling model.train() will disable the dropout layers.
+        model.eval()
 
-    history['val_loss'].append(val_loss)
-    history['val_acc'].append(val_acc)        
-    
-    t1 = time.time()
+        for batch in valid_batches:
+            scores = model(batch.text)
+            n_corr_batch, loss_batch = evaluate_validation(scores, loss_function, batch.label)
+            loss_sum += loss_batch
+            n_correct += n_corr_batch
+            n_batches += 1
+        val_acc = n_correct / n_valid
+        val_loss = loss_sum / n_batches 
+        t1 = time.time()
 
-    if (i+1) % 10 == 0:
-        print(f'Epoch {i+1}: train loss = {train_loss:.4f}, val loss = {val_loss:.4f}, val acc: {val_acc:.4f}, time = {t1-t0:.4f}')
-    
-    output_file = "./NH_CBoW_{}.pt".format(epoch_i + 1)
+        if (i+1) % 10 == 0:
+            print(f'Epoch {i+1}: train loss = {train_loss:.4f}, val loss = {val_loss:.4f}, val acc: {val_acc:.4f}, time = {t1-t0:.4f}')
+        
+    output_file = f"./NH_CBoW.pt"
     torch.save(model.state_dict(), output_file)
+
+
